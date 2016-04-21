f7ba75a850f1533c26359adefbffe54c5cb026b2
akircher <adam.kircher@gmail.com>
2016-04-21 14:03:23 -0700

    Return full doc on shipments.post()

--- shipments.js
+++ shipments.js
@@ -1,12 +1,22 @@
+"use strict"
+
 exports.post = function* () { //TODO querystring with label=fedex creates label, maybe track=true/false eventually
-  yield this.couch.put({proxy:true})
-  .url(body => `/${body.account.from._id}.${body.account.to._id}.${this.couch.id()}`)
+  let res = yield this.couch.put()
+  .url(body => `/shipments/${body.account.from._id}.${body.account.to._id}.${this.couch.id()}`)
   .body(body => {
     //TODO replace this with an Easy Post API call that actually creates a label
     //TODO create pickup for the next business date
     body.tracking  = Math.floor(Math.random() * (99999-10000))+10000,
     body.createdAt = new Date().toJSON()
+    this.body = body
   })
+  this.status = res.status
+
+  if (this.status != 201)
+    return this.body = res.body
+
+  this.body._id  = res.body.id
+  this.body._rev = res.body.rev
 }
 
 exports.shipped = function* (id) {
