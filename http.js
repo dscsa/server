7ae2700468943b9cb46ec9c1a2d80c816e2b1987
akircher <adam.kircher@gmail.com>
2016-09-11 23:41:01 -0700

    Refactor HTTP
    Closes dscsa/client#186

--- http.js
+++ http.js
@@ -1,197 +1,175 @@
 "use strict"
-let http  = require('http')
-let url   = require('url')
+let httpRequest = require('http').request
+let urlParse    = require('url').parse
+let qsString    = require('querystring').stringify
 
 let count = 0, old, time //for use with couch.id
 
-module.exports = defaults => {
+//Since http is ansyncronous, we throw more meaningful errors by
+//saving the stack when API is run and then appending it at end
+function asyncError(err, status) {
+  err = {err}
+  err.status = status
+  err = Error(JSON.stringify(err))
+  err.stack += httpFactory.stack
+  return err
+}
 
-  if ( ! defaults.middleware)
-    return init(defaults, {})
+function parseUrl(url) {
+  if (typeof url == 'string') //settings is already be an object
+    url = urlParse(url, true)
+
+  if (url.pathname && url.pathname[0] != '/')
+    url.pathname = '/'+url.pathname //force relative paths to be absolute
+
+  return {
+    //protocol:url.protocol, //TODO node http.request does not support https, need to https module
+    host:url.host,
+    pathname:url.pathname,
+    query:url.query
+  }
+}
+
+function getDefaults(userUrl = {}, defaultUrl = {}, ctxUrl = {}) {
+
+  //Object assign will overwrite with null, so we must do manually
+  let protocol = userUrl.protocol || defaultUrl.protocol || ctxUrl.protocol
+  let host  = userUrl.host || defaultUrl.host || ctxUrl.host
+  let path  = userUrl.pathname || defaultUrl.pathname || ctxUrl.pathname
+  let query = userUrl.query || defaultUrl.query || ctxUrl.query
+
+  //TODO do not forward headers if user.host is specified either, right now getGoodRx & getNadac must set .headers()
+  if ( ! userUrl.host) //Only proxy querystrings if user does not specify a host
+    query = Object.assign({}, ctxUrl && ctxUrl.query, defaultUrl.query, userUrl.query)
+
+  let [hostname, port] = host.split(':') //http.request needs "port" prop
+  path += '?'+qsString(query)            //http.request needs "path" prop
+
+  return { hostname, port, path } //protocol
+}
+
+function getHeaders(userHeaders, defaultHeaders, ctxHeaders) {
+  let headers = Object.assign({}, ctxHeaders, defaultHeaders, userHeaders)
+  delete headers['content-length']
+  return headers
+}
+
+function makeConfig(user, settings, ctx) {
+  let config = getDefaults(parseUrl(user.url || ''), settings.parsedUrl, ctx.parsedUrl)
+  config.headers = getHeaders(user.headers, settings.headers, ctx.headers)
+  config.method  = (user.method || settings.method || ctx.method).toUpperCase()
+  config.body    = user.body || settings.body || ctx.body
+  return Promise.resolve(config)
+}
+
+module.exports = settings => {
+
+  let middleware = settings.middleware
+
+  settings = {
+    headers    : settings.headers,
+    body       : settings.body,
+    parsedUrl  : parseUrl(settings)
+  }
+
+  if ( ! middleware)
+    return httpFactory(settings)
 
   return function *(next) {
-    this[defaults.middleware] = init(defaults, this)
+    this.body       = this.req,
+    this.parsedUrl  = parseUrl(this.url)
+
+    this[middleware] = httpFactory(settings).context(this)
     yield next
   }
 }
 
+function httpFactory(settings) {
+  let ctx = {}
 
+  function http(url, body) {
+    httpFactory.stack = Error().stack
 
-function init(defaults, ctx) {
+    let user = {url, body, proxy:true}, api = {
+      method(method) {
+        user.method = method
+        return api
+      },
 
-  function parseUrl(path) {
-    path = path || {}
-
-    if (typeof path == 'string') {
-      path = url.parse(path)
-
-      //if path is string and hostname is empty (goodrx and nadac queryies
-      //shouldn't use original url's qs) then combine path's qs with original qs
-      if (ctx.querystring && ! path.hostname) {
-        path.path += path.path.includes('?') ? '&' : '?'
-        path.path += ctx.querystring
-      }
-
-      if (path.path[0] != '/') //force relative paths to be absolute
-        path.path = '/'+path.path
-    }
-
-    return  {
-      hostname:path.hostname || defaults.hostname,
-      port:path.port || (path.hostname ? null : defaults.port),
-      method:(path.method || ctx.method || 'get').toUpperCase(),
-      path:path.path || ctx.url, //this includes querystring
-      headers:path.headers || ctx.req && ctx.req.headers,
-      body:path.body || ctx.req
-    }
-  }
-
-  //Right now {parse:true} is only option, eventually we can suppose {forms, json, multipart}
-  function api(path, body) {
-
-    let proxy = true, stack = new Error().stack, config = parseUrl(path)
-
-    let methods = {
       headers(headers) {
-        config.headers = headers || {}
-        return this
+        user.headers = headers || {} //headers cannot be undefined or request will hang
+        return api
       },
 
-      body:{
-        then(a,b) {
-          proxy = false
-          return methods.then(res => api.json(res)).then(a,b)
-        }
+      catch(reject) {
+        return api.then(null, reject)
       },
 
-      then(a, b) {
-        return Promise.all([config.headers, config.body])
-        .then(all => {
-
-
-          config.headers = JSON.parse(JSON.stringify(all[0]))
-          config.body    = all[1]
-
-
-          //Make sure not a reference to response headers otherwise background tasks like
-          //drugs.updatePrice will throw an headers already sent error once the response it set
-          if (config.headers && ! ctx.headerSent) {
-            delete config.headers['content-length']
-            config.headers['content-type'] = 'application/json' //seemed to be needed for _session login
-            config.headers.accept = 'application/json'  //TODO is there a better way to not get multipart messages from couchdb
-          }
-
-          var req = http.request(config)
-
-          if(config.method == 'GET' || (config.method == 'DELETE' && ! config.body))
-            req.end('')  //Don't drain req body with GET request, Delete requests have optional body
-
-          else if ( ! config.body)
-            console.log(`Error: you forgot to set request body for ${config.method} ${config.path}`, config)
-
-          else if ( ! config.body.pipe)
-              req.end(JSON.stringify(config.body))
-
-          else if ( ! config.body.readable)
-            console.log(`Error: body's stream for ${config.method} ${config.path} is no longer readable.  Maybe you forgot to set the body to something else?`)
-
-          else
-            config.body.pipe(req, {end: true})
-
-          return new Promise((resolve, reject) => {
-            req.once('response', resolve)
-            req.once('error', reject)
-          })
-        })
-        .then(res => {
-
-          ctx.status = res.statusCode
-
-          if (ctx.status < 200 || ctx.status >= 300)
-            throw api.json(res)
-
-          if (! proxy)
-            return res
-
-          ctx.set && ! ctx.headerSent && ctx.set(res.headers)
-          ctx.body = res
-        })
-        .catch(err => {
-            err.stack = stack
-            throw err
-        })
-        .then(a,b)
-      },
-
-      catch(b) {
-        return this.then(null, b)
+      then(resolve, reject) {
+        return makeConfig(user, settings, ctx)
+          .then(request)
+          .then(response.bind({proxy:user.proxy}))
+          .then(resolve, reject)
       }
     }
 
-    return methods
+    Object.defineProperty(api, 'body', {
+      get() {
+        user.proxy = false
+        return api
+      }
+    })
+
+    return api
   }
 
-  api.get = path => {
-    path = parseUrl(path)
-    path.method = 'get'
-    return api(path, body)
+  http.context = context => {
+    ctx = context
+    return http
   }
 
-  api.post = (path, body) => {
-    path = parseUrl(path)
-    path.method = 'post'
-    return api(path, body)
-  }
+  http.get = path => http(path).method('get')
 
-  api.put = (path, body) => {
-    path = parseUrl(path)
-    path.method = 'put'
-    return api(path, body)
-  }
+  http.post = (path, body) => http(path, body).method('post')
 
-  api.delete = (path, body) => {
-    path = parseUrl(path)
-    path.method = 'delete'
-    return api(path, body)
-  }
+  http.put = (path, body) => http(path, body).method('put')
 
-  api.json = stream => {
+  http.delete = (path, body) => http(path, body).method('delete')
 
-    let stack = new Error().stack
+  http.json = stream => {
 
     if (stream.body) //maybe this stream has already been collected.
       return Promise.resolve(stream.body)
 
     if (typeof stream.on != 'function') //ducktyping http://stackoverflow.com/questions/23885095/nodejs-check-if-variable-is-readable-stream
-      return console.log('http.json was not given a stream', stack, stream)
+      throw asyncError('http.json was not given a stream')
 
     if ( ! stream.readable)
-      return console.log('http.json stream is already closed', stack, stream)
+      throw asyncError('http.json stream is already closed')
 
     stream.body = ''
     return new Promise((resolve, reject) => {
-      stream.on('error', reject)
+      stream.on('error', err => reject(asyncError(err)))
       stream.on('data', data => stream.body += data)
       stream.on('end', _ => {
         try {
           resolve(JSON.parse(stream.body || '{}'))
         } catch (err) {
-          err.stack   = stack
-          reject(err)
-          console.log('Invalid JSON', stream.body)
+          reject(asyncError('Error: Invalid JSON '+stream.body))
         }
       }) //default to {} this is what other body parsers do in strict mode.  Not sure what we want to do here.
     })
   }
 
-  api.body = {
-    then(a,b) {
-      return api.json(ctx.body || ctx.req).then(a,b)
+  //Return a 7 digit uuid that can handle upto 36^2/47 = 27 requests per second
+  Object.defineProperty(http, 'body', {
+    get() {
+      return http.json(ctx.body)
     }
-  }
+  })
 
   //Return a 7 digit uuid that can handle upto 36^2/47 = 27 requests per second
-  Object.defineProperty(api, 'id', {
+  Object.defineProperty(http, 'id', {
     get() {
       old   = time
       time  = Date.now().toString(36).slice(-8, -3) //5 digit timestamp. Updates ever 36^3/1000 = 47 seconds and doesn't repeat for 36^8/1000 = 89 years
@@ -200,5 +178,52 @@ function init(defaults, ctx) {
     }
   })
 
-  return api
+  return http
+
+  function request(config) {
+    //console.log('httpRequest', config.protocol, config.hostname, config.port, config.path)
+    var req = httpRequest(config)
+
+    if(config.method == 'GET')
+      req.end('')  //Don't drain req body with GET request
+
+    else if ( ! config.body) //Delete requests have optional body, put for couch db creation is another without body
+      req.end('') && console.warn(`You forgot to set request body for ${config.method} ${config.path}`)
+
+    else if ( ! config.body.pipe)
+      req.end(JSON.stringify(config.body))
+
+    else if ( ! config.body.readable)
+      throw asyncError(`body's stream for ${config.method} ${config.path} is no longer readable.  Did you forget to set the body?`)
+
+    else
+      config.body.pipe(req, {end: true})
+
+    return new Promise((resolve, reject) => {
+      req.once('response', resolve)
+      req.once('error', reject)
+    })
+  }
+
+  function response(res) {
+    if (ctx.set && ! ctx.headerSent)
+      ctx.status = res.statusCode //Always proxy the status code
+
+    //console.log('http response', res.req.method, res.req.path, res.statusCode, res.statusMessage, this.proxy)
+    if (res.statusCode >= 500)
+      return http.json(res).then(body => {
+        throw asyncError(body, res.statusCode)
+      })
+
+    if ( ! this.proxy && (res.statusCode < 200 || res.statusCode >= 300))
+      throw http.json(res)
+
+    if ( ! this.proxy)
+      return http.json(res)
+
+    if (ctx.set && ! ctx.headerSent) {
+      ctx.set(res.headers)
+      ctx.body = res
+    }
+  }
 }
