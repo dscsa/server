b12f0c97fe07da8c5f6f7ca3c412cb5250ba85b8
akircher <adam.kircher@gmail.com>
2016-10-22 01:44:52 -0700

    Try _id fix again

--- http.js
+++ http.js
@@ -168,12 +168,13 @@ function httpFactory(settings, ctx = {}) {
   //Return a 7 digit uuid that can handle upto 36^2/47 = 27 requests per second
   Object.defineProperty(http, 'id', {
     get() {
-      old   = time
-      time  = Date.now().toString(36).slice(-8, -3) //5 digit timestamp. Updates ever 36^3/1000 = 47 seconds and doesn't repeat for 36^8/1000 = 89 years
+      old  = time
+      time = Date.now().toString(36).slice(-8, -3) //5 digit timestamp. Updates ever 36^3/1000 = 47 seconds and doesn't repeat for 36^8/1000 = 89 years
+
       if (count >= 36 * 36) {
-        old = old.slice(0, -1)+String.fromCharCode(old.charCodeAt(old.length-1) + 1)
+        time = old.slice(0, -1)+String.fromCharCode(old.charCodeAt(old.length-1) + 1)
       }
-      count = time <= old ? count+1 : 0
+      count = time == old ? count+1 : 0
 
       return time+("00"+count.toString(36)).slice(-2) //Force Id to 2 digits (36^2 updates per time period).  Overall uuid is 7 digits
     }
