927857a7cf90feadf55ec52e2c5059ad465867af
akircher <adam@Adams-MacBook-Pro-2.local>
2019-03-10 16:31:41 -0400

    Try to prevent timeouts

--- models/account.js
+++ models/account.js
@@ -168,6 +168,9 @@ exports.recordByFrom = async function (ctx, to_id) { //account._id will not be s
 
 exports.recordByUser = async function  (ctx, to_id) { //account._id will not be set because google does not send cookie
 
+
+  ctx.res.write(' ') //attempt to keep browser/gsheets from timing out on long request_timeout
+  
   //If group_level by From or Shipment, let's add in some demornalized accout data that we can use in the V1 & V2 Merge gSheet
   //Baseline is group by [to_id, user], we need at least [to_id, user, from] in order to add account data.
   //NULL group_level will just result in a negative integer
