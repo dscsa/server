096ca49f03c66286cfb30b0466d06ad108a5d25b
akircher <adam@Adams-MacBook-Pro-2.local>
2019-09-22 02:41:49 -0400

    Allow old user ids within transactions

--- models/_users.js
+++ models/_users.js
@@ -4,7 +4,7 @@
 exports.validate = function(model) {
   return model
    .ensure('_id').set(doc => 'org.couchdb.user:'+doc.name)
-   .ensure('name').required().minLength(21).maxLength(21) //user_id.account_id
+   .ensure('name').required().pattern(/^\d{10}$|^\d{10}\.\d{10}$/) //user_id.account_id
    .ensure('type').set(doc => 'user')
    .ensure('roles').typeArray().minLength(2).maxLength(2) //allAccounts, and account._id
 }
